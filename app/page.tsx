905ca03394fc2f9e7aea50b4d0fa67d42c11ef6f
Solractys <135873875+Solractys@users.noreply.github.com>
2025-01-08 16:58:41 -0300

    api test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
+"use client";
+import React, { useEffect, useState } from "react";
+import { searchBooks } from "./api/books";
+
+function Home() {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const response = searchBooks("Jane Austen");
+    setBooks(response);
+    console.log(books);
+  }, []);
   return (
     <main>
-      <div>Hello world!</div>
+      <div>
+        {books.length > 0 ? (
+          <ul>
+            {books.map((book) => (
+              <li key={book.id}>{book.volumeInfo.title}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No books found</p>
+        )}
+      </div>
     </main>
   );
 }
+export default Home;
