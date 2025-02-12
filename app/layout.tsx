6a1ed7dee2d95cef5635e064708941203dc4fcbb
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-02-12 12:20:00 -0300

    Change the page name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Quiz Book",
+  title: "Who Wrote Me",
   description: "A quiz book for everyone",
 };
 
